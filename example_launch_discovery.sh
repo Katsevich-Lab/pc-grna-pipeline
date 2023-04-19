e120d1984f7e46e81d2383bae4de05f1d172d313
Tim <tbarry2@andrew.cmu.edu>
2023-04-18 22:25:50 -0400

    add ex bash script for disc analysis

---
+++ example_launch_discovery.sh
@@ -0,0 +1,9 @@
+source ~/.research_config
+
+nextflow run $LOCAL_CODE_DIR"/pc-grna-pipeline/main.nf" \
+ --data_method_pair_file $LOCAL_CODE_DIR"/sceptre2-manuscript/param_files/params_discovery_0423.groovy" \
+ --grna_modality "assignment" \
+ --result_dir $LOCAL_SCEPTRE2_DATA_DIR"/results/discovery_analyses" \
+ --result_file_name "discovery_results_0423.rds" \
+ --trial "false" \
+ --discovery "true"
